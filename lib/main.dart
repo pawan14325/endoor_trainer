9988c90bb7a2390a325fcf73aa5bd6f66779b1a3
pawan14325 <pawanpundir921@gmail.com>
2025-02-17 18:29:34 +0530

    login events

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,7 @@ class EndoorphinTrainer extends StatelessWidget {
       title: 'Endoorphin Trainer',
       themeMode: ThemeMode.dark,
       theme: AppThemes.appTheme,
-      initialRoute: RoutesName.home,
+      initialRoute: RoutesName.login,
       // Initial route
       onGenerateRoute: Routes.generateRoute,
       builder: (context, child) {
--- lib/presentations/login/bloc/login_state.dart
+++ lib/presentations/login/bloc/login_state.dart
@@ -5,3 +5,13 @@ abstract class LoginState {}
 
 class LoginInitial extends LoginState {}
 
+
+
+class LoginLoading extends LoginState {}
+
+class LoginSuccess extends LoginState {}
+
+class LoginError extends LoginState {
+  final String message;
+  LoginError(this.message);
+}
